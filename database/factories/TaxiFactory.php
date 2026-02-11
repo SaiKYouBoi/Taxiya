7d9582a8792e6e50d25c14ef92d57ff653ecd644
yassinmftah <yassinmaftah06@gmail.com>
2026-02-11 12:53:40 +0100

    add user_id in TaxiFactory

--- database/factories/TaxiFactory.php
+++ database/factories/TaxiFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Taxi>
  */
@@ -23,6 +23,7 @@ class TaxiFactory extends Factory
             'vehicule_make' => fake()->company(),
             'year_manufacture' => fake()->year(),
             'taxi_mumber' => fake()->unique()->numberBetween(100, 9999),
+            'user_id' => User::inRandomOrder()->value('id') ?? User::factory(),
         ];
     }
 }
