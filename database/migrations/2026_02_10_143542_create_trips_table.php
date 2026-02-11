53df1455e29f70229d1aa30cbe7a96b2ab1475ce
zakariadachi <dachiziko@gmail.com>
2026-02-11 11:38:25 +0100

    delete foreign key of recurence

--- database/migrations/2026_02_10_143542_create_trips_table.php
+++ database/migrations/2026_02_10_143542_create_trips_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->foreignId('departure_city_id')->constrained('cities');
             $table->foreignId('arrival_city_id')->constrained('cities');
             $table->foreignId('taxi_id')->constrained('taxis');
-            $table->foreignId('recurrence_id')->nullable()->constrained('recurrences');
             $table->dateTime('departure_datetime');
             $table->decimal('base_price', 10, 2);
             $table->enum('status', ['waiting', 'ongoing', 'completed', 'cancelled'])->default('waiting');
