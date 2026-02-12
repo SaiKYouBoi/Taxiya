47c8ebb128ca08278376062682f30f1292d17ad1
zakarya hari <zakariahari42@gmail.com>
2026-02-12 15:05:05 +0100

    Correction: Renommer Driver() en driver() dans le modèle Taxi

--- app/Models/Taxi.php
+++ app/Models/Taxi.php
@@ -13,9 +13,9 @@ class Taxi extends Model
 
     protected $fillable = ['license_plate', 'model', 'vehicule_make', 'year_manufacture', 'taxi_mumber', 'user_id'];
 
-    public function Driver()
+    public function driver()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function Trip()
