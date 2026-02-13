58d431405765964e1e4c75d10dd4ad0a46add678
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-13 16:11:54 +0100

    Added admin validatio apprive and reject driver

--- app/Http/Controllers/Admin/DriverValidationController.php
+++ app/Http/Controllers/Admin/DriverValidationController.php
@@ -12,10 +12,36 @@ class DriverValidationController extends Controller
     {
         $drivers = User::with('taxi')
             ->where('role', 'driver')
-            ->where('is_validated', false) 
+            ->where('is_validated', false)
             ->latest()
             ->get();
 
         return view('admin.drivervalidation', compact('drivers'));
     }
+
+    public function approve(User $driver){
+
+
+        if ($driver->role !== 'driver') {
+            abort(403);
+        }
+
+        $driver->update([
+            'is_validated' => true,
+        ]);
+
+        return redirect()->back()->with('success', 'Driver approved successfully.');
+    }
+
+    public function reject(User $driver){
+
+        if ($driver->role !== 'driver') {
+        abort(403);
+    }
+
+    $driver->delete();
+
+    return redirect()->back()->with('success', 'Driver rejected successfully.');
+    }
+
 }
