146ce31f1ef602a3db7bfe9752b304f6feeefe7a
khadijaabirat <khadijaabirat@gmail.com>
2026-02-13 09:57:21 +0100

    Added bookings controller changes to match bookings logic

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,11 +6,9 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-     public function DriverBookings(){
-     $driver = auth()->user()->load(['taxi.trips.bookings.traveler']);
 
-     $trips = $driver->taxi ? $driver->taxi->trips : collect();
+ 
 
-    return view('driver.bookings', compact('trips'));
-     }
+
+
 }
