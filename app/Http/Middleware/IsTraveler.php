cf1473fd484da59cb1f575841131f0d330dfcdd6
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-17 12:55:08 +0000

    added isTraveler middleware

---
+++ app/Http/Middleware/IsTraveler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsTraveler
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $user = $request->user();
+
+        if($user->role !== 'traveler'){
+            return redirect()->back();
+        }
+
+        return $next($request);
+    }
+}
